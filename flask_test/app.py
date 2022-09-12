74942ded1f6a2f0fba03cb8ed2bde35bf99dad67
gahoo <ilue00@naver.com>
2022-09-12 19:20:02 +0900

    flask 도커 성공

---
+++ flask_test/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+html = '''
+<h1>도커 테스트</h1>
+<p>도커 쉽게 하기 마스터</p>
+'''
+
+@app.route("/index")
+def index():
+    return html
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5005)
