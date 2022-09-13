65270823defc4d5739c6f20d133835b20ca0b2e6
gahoo <ilue00@naver.com>
2022-09-13 19:49:04 +0900

    nginx까지 잘됨

--- flask_test/app.py
+++ flask_test/app.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-html = '''
-<h1>도커 테스트</h1>
-<p>도커 쉽게 하기 마스터</p>
-'''
+
+app.config['MONGO_URI'] = "mongodb://localhost:27017/dockerTest"
+
+mongo = PyMongo(app)
+
+# html = '''
+# <h1>도커 테스트</h1>
+# <p>도커 쉽게 하기 마스터</p>
+# '''
+# html2 = '''
+# <h1>HTML2 테스트</h1>
+# <p>도커 쉽게 하기 마스터</p>
+# '''
+
 
 @app.route("/")
 def index():
-    return html
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5005)
